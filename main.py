be01a89cb0e0e034361ff09179557a6579529e5e
Eightbitthero <rubiconforrest@gmail.com>
2024-12-27 15:49:16 -0700

    o

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from tkinter import Tk, BOTH, Canvas
-from Window import Window
+
+from Window import Window, Line, Point
 
 def main():
-    win = Window(800, 600) 
+    win = Window(800, 600)
+    l = Line(Point(50,50), Point(400, 400))
+    win.draw_line(l, "black")
     win.wait_for_close()
 
-main ()
+
+main()
